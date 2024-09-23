3cbd23c1f105a9f66d7afc62c6774a840dc97805
Madina-j <dzhetegenova@gmail.com>
2024-09-23 15:58:27 -0700

    add in party.py  named all functions with pass from README.md

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -16,24 +16,50 @@ def add_to_watched(user_data, movie):
     print(len(user_data["watched"]))
     return user_data
     
-
-def add_to_watchlist(user_data,title):
+# No.3
+def add_to_watchlist(user_data,movie):
     pass
+# No.4
+def watch_movie(user_data,title):
+    pass
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+# No.1
+def get_watched_avg_rating(user_data):
+    pass
+
+# No.2
+def get_most_watched_genre(user_data):
+    pass
+
 
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-        
+# No.1
+def get_unique_watched(user_data):
+    pass
+# No.2
+def get_friends_unique_watched(user_data):
+    pass
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+# No.1
+def get_available_recs(user_data):
+    pass
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
+# No.1
+def get_new_rec_by_genre(user_data):
+    pass
 
+# No.2 
+def get_rec_from_favorites(user_data):
+    pass
