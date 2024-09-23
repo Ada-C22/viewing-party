315f252ef3b3b17e456d3121b6d7d59dbc9b16f1
Christelle Nkera <scnkera@gmail.com>
2024-09-23 14:55:13 -0400

    Created a movie dictionary

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,6 +1,50 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
+    movie_dict = {}
+
+    if not title or not genre or not rating:
+        print(None)
+        return None
+    else:
+        movie_dict['title'] = title
+        movie_dict['genre'] = genre
+        movie_dict['rating'] = rating
+        print(movie_dict)
+    
+    return movie_dict
+
+create_movie("happy feet", "Drama", 5)
+create_movie("", "Drama", 5)
+
+
+
+# def add_to_watched(user_data, movie):
+#     # user_data 
+#     # empty list = no movies wathced
+#     user_data = {
+#         "watched" : [
+#             {
+
+#             }
+#         ]
+#     }
+#     # return the user_data
+
+#     user_data = {}
+
+#     movie_info = create_movie(title, genre, rating)
+
+#     user_data["watched"] = 
+
+
+
+
+
+def add_to_watchlist(user_data, movie):
+    pass
+
+def watch_movie(user_data, title):
     pass
 
 # -----------------------------------------
