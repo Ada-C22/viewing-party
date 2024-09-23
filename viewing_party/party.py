b093a87a4ee56f316f7be71e0de49cc53728ed03
Beenish <beenishali693@gmail.com>
2024-09-23 14:41:56 -0400

    Changes from 1st session together

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,30 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if not title or not genre or not rating:
+        return None 
+    movie_dict = {}
+    if type(title) == str and type(genre) == str and type(rating) == int or type(rating) == float:
+        movie_dict["title"] = title
+        movie_dict["genre"] = genre
+        movie_dict["rating"] = rating
+    else:
+        return None
+    return movie_dict
 
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    if title in user_data["watchlist"]:
+        user_data["watched"].append(title)
+        user_data["watchlist"].pop(title)
+    return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
