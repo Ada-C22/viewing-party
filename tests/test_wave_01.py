eba0c6f0431910ce02a577557e4be5e13a7000c0
Sarah Koch <sarahkoch810@gmail.com>
2024-09-23 16:33:22 -0700

    Removes raised exception and skip decorator from test_moves_movie_from_watchlist_to_empty_watched() in test_wave_01.py, adds assert statements to test that the correct movie was added to "watched"

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -140,7 +140,7 @@ def test_adds_movie_to_non_empty_user_watchlist():
     assert movie in updated_data["watchlist"]
     assert FANTASY_2 in updated_data["watchlist"]
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_empty_watched():
     # Arrange
     janes_data = {
@@ -158,11 +158,9 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
-    
-    raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    assert updated_data["watched"][0]["title"] == MOVIE_TITLE_1
+    assert updated_data["watched"][0]["genre"] == GENRE_1
+    assert updated_data["watched"][0]["rating"] == RATING_1
 
 @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
