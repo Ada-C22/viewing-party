d91d2e6289f31b24a7deda2c73961b0dba8e97f6
Leidy <leidymarbu@gmail.com>
2024-09-23 16:03:11 -0400

    Test passed in create_movie func wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,20 @@
-# ------------- WAVE 1 --------------------
+#
+##------------- WAVE 1 --------------------
+def create_movie(movie_title, genre, rating):
+    movie= {}
 
-def create_movie(title, genre, rating):
-    pass
+    if movie_title and genre and rating:
+        
+        if isinstance(movie_title,str) or isinstance(genre,str)\
+        or rating.isinstance(rating (int,float)):
+            
+            movie["title"] = movie_title
+            movie["genre"] = genre
+            movie["rating"] = rating          
+                
+        return movie
+    
+    return None
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
