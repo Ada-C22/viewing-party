13dd493400026085e3c1098ac29a76db9091a33c
Charday Neal <charday.neal@gmail.com>
2024-09-23 12:00:22 -0700

    Complete tests_moves_movie_from_watchlist_to_empty_watched function

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -159,10 +159,11 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
     
-    raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    assert updated_data["watched"] == [{
+            "title": MOVIE_TITLE_1,
+            "genre": GENRE_1,
+            "rating": RATING_1
+        }]
 
 @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
