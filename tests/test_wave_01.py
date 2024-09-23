c9d710d7672236e081066fd73d429df100127001
Sarah Koch <sarahkoch810@gmail.com>
2024-09-23 16:39:52 -0700

    Modifies test_wave_01.py function test_move_movie_from_watchlist_to_watched(). Adds assertions to test that the correct movie was added to "watched". Removes raised exception and comments out skip decorator.

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -162,7 +162,7 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert updated_data["watched"][0]["genre"] == GENRE_1
     assert updated_data["watched"][0]["rating"] == RATING_1
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -180,11 +180,9 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
-    
-    raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    assert updated_data["watched"][1]["title"] == movie_to_watch
+    assert updated_data["watched"][1]["genre"] == GENRE_1
+    assert updated_data["watched"][1]["rating"] == RATING_1
 
 # @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
