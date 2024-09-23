c019a01821ccca1f4286786621215cceac61cf04
Tatyana Venanzi <tatyanavenanzi@gmail.com>
2024-09-23 16:01:08 -0700

    Completed remaining functions under wave 1 and passed all tests in test_wave_01.py

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,36 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if not title or not genre or not rating:
+        return None
+    
+    return {
+        "title": title,
+        "genre": genre,
+        "rating": rating
+    }
+    # check if parameters are truthy and if so return empty_dict
+    # if falsy return none
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    # Loop through watchlist dict
+    for movie in user_data["watchlist"]:
+        # check if the tile of movie is in the user_data dict
+        if title == movie["title"]:
+            # remove the movie from watchlist if user already watched it
+            user_data["watchlist"].remove(movie)
+            # add movie to watched list if user already watched it
+            user_data["watched"].append(movie)
+            break # exit the loop once movie is found and moved
+    return user_data # return updated dict
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
