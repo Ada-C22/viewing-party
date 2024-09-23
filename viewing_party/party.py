b3a7eabda907fa404186154943fb22253ea01c68
Tatiana Snook <tatsnook@gmail.com>
2024-09-23 13:37:03 -0500

    partial wave 1 is completed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,21 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    new_movie = {
+        "title": title,
+        "genre": genre,
+        "rating": rating
+    }
+
+    if title is None or genre is None or rating is None:
+        return None
+
+    return new_movie
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
