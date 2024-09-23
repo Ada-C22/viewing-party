72d0f963437e03c26d6a01bfbfe659b5f1cb7fc8
Alejandra Guevara <alejandra.gue91@gmail.com>
2024-09-23 19:03:52 -0400

    Test passed for wave1 function1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,16 @@
 # ------------- WAVE 1 --------------------
+from tests.test_constants import *
 
 def create_movie(title, genre, rating):
-    pass
+    if not title or genre or rating:
+        return None 
+    movie = {
+        "title": MOVIE_TITLE_1,
+        "genre": GENRE_1,
+        "rating": RATING_1 ,
+    }
+    return movie 
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
