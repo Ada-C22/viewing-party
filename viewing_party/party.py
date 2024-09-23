0bf6e2da44dd2e7bed69aa40fbda2ef5ed959338
Tami <tamigaertner@outlook.com>
2024-09-23 16:19:39 -0400

    work on wave 1 create_movie and add_to_watchlist function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,42 @@
 # ------------- WAVE 1 --------------------
-
+#
 def create_movie(title, genre, rating):
-    pass
+    
+    if title and genre and rating:
+        return {
+            "title": title,
+            "genre": genre,
+            "rating": rating,
+        }
+    else:
+        return None
+# print(create_movie("movie_title", "comedy", "5"))
+# user_data:
+# {"watched": [movies] 
+#  "watchlist": [movies]} <-- add here a movie
+# add movie to 'watched' list from the user dict
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+    # print(user_data)   
 
+# usr = {
+#         "watched": [
+#             "FANTASY_1",    
+#             "FANTASY_3",
+#             "FANTASY_4",
+#             "HORROR_1",
+#         ]
+#     ,
+#         "watchlist": [
+#             "FANTASY_2",
+#             "ACTION_2",
+#             "INTRIGUE_2",
+#             "INTRIGUE_2",
+#         ]
+#     }
+
+# add_to_watchlist( usr, "soe " )
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
