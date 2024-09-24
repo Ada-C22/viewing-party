b0d65cf13399d3d699985df930dab9bc677922f7
Carla Boyd <cardin_23_2@hotmail.com>
2024-09-23 20:04:26 -0700

    "Passes all tests in wave 2"

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -38,15 +38,62 @@ def watch_movie(user_data, title):
     return movies
 
 
-# create_movie("Title A", "Horror", 3.5)
-# movie ="Title A", "Horror", 3.5
-# add_to_watched(user_data, movie)
-# add_to_watchlist(user_data, movie)
-# print (user_data)
-
-# -----------------------------------------
 # ------------- WAVE 2 --------------------
-# -----------------------------------------
+def get_watched_avg_rating(user_data):
+    ratings = 0
+    movie_counter = 0
+    avg_rating = 0
+    
+    if user_data["watched"]:
+        for movie in user_data["watched"]:
+            if "rating":
+                ratings += movie["rating"]
+                movie_counter += 1
+            else:
+                movie_counter += 1
+        return ratings/movie_counter
+    return 0.0
+
+def get_most_watched_genre(user_data):
+    genres = {}
+    frecuency_data = []
+    if user_data["watched"]:
+        for movie in user_data["watched"]:
+            if "genre":
+                if movie["genre"] not in genres:
+                    genres[movie["genre"]] = 1
+                else:
+                    genres[movie["genre"]] += 1
+        for genre, frecuency in genres.items():
+            frecuency_data.append(frecuency)
+        for genre, frecuency in genres.items():
+            if frecuency == max(frecuency_data):
+                return genre 
+        
+        
+    return None
+
+# user_data = {   'watched': [   {   'genre': 'Fantasy',
+#                     'rating': 4.8,
+#                     'title': 'The Lord of the Functions: The Fellowship of '
+#                                 'the Function'},
+#                 {   'genre': 'Fantasy',
+#                     'rating': 4.0,
+#                     'title': 'The Lord of the Functions: The Two '
+#                                 'Parameters'},
+#                 {   'genre': 'Fantasy',
+#                     'rating': 4.0,
+#                     'title': 'The Lord of the Functions: The Return of the '
+#                                 'Value'},
+#                 {   'genre': 'Action',
+#                     'rating': 2.2,
+#                     'title': 'The JavaScript and the React'},
+#                 {'genre': 'Intrigue', 'rating': 2.0, 'title': 'Recursion'},
+#                 {   'genre': 'Intrigue',
+#                     'rating': 4.5,
+#                     'title': 'Instructor Student TA Manager'}]}
+
+# get_watched_avg_rating(user_data)
 
 
 # -----------------------------------------
@@ -60,5 +107,4 @@ def watch_movie(user_data, title):
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
-# -----------------------------------------
-
+# ----------------------------------------
