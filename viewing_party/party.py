4ba72f69728c80e00077a04ea6dae1282a59f89c
Tami <tamigaertner@outlook.com>
2024-09-23 20:01:21 -0400

    work on test_wave.01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -19,7 +19,7 @@ def create_movie(title, genre, rating):
 def add_to_watched(user_data, movie):
     user_data["watched"].append(movie)
     return user_data
-   
+
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
