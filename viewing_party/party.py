2d573ee03005fd9f2771498955815d1d5717792b
Natalie <natali3sen@gmail.com>
2024-09-24 18:43:53 -0500

    wrote the first function of wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -31,7 +31,29 @@ def watch_movie(user_data, title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    # use the .get() method to get the watched list from user_data
+    # check to see if the list is empty then return 0.0
+    # initialize the total rating and count all movies in the dict
+    # loop through each movies in watched_movies to get the ratings
+    # add each rating to total_rating
+    # calculate the average rating by dividing the total rating by the movie counts
+    # return the average rating 
 
+    watched_movies = user_data.get("watched", [])
+
+    if not watched_movies:
+        return 0.0
+    
+    total_rating = 0.0
+    all_movies = len(watched_movies)
+
+    for movie in watched_movies:
+        total_rating += movie["rating"]
+
+    avg_rating = total_rating / all_movies
+
+    return avg_rating
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
