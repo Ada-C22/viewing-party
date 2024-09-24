26373fe775c8c4037ec98dd3c909b0a5e5be9e67
Anees Quateja <aneesq87@gmail.com>
2024-09-24 13:40:12 -0700

    my changes

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,26 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
 
+    # dict_1 = {}
+    
+    # if title and genre and rating == True:
+    #     return dict_1
+    
+    if create_movie is True:
+        return create_movie
+    else:
+        return None
+
+
+# def add_to_watched(user_data, movie):
+
+    user_data = {}
+    watched = True
+    for things in user_data:
+        for watch_key, value_value in things:
+            if watch_key and value_value == watched:
+                return  
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
