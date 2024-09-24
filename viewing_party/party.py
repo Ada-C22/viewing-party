83883813d4199f3afbe5e644db4dec44d65ae1a5
Salma Anany <salmayousry5@gmail.com>
2024-09-23 19:45:56 -0700

    step1 in wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,17 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title is None or genre is None or rating is None:
+        return None
+    return {'title': title, 'genre': genre, 'rating': rating}
+
+
+
+
+
+
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
