380a477ba828c55297d5110b1ce76bd36ccf2795
Aksana <adziamidava@gmail.com>
2024-09-23 17:21:51 -0700

    wrote create_movie() add_to_watched() add_to_watchlist functions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,16 +1,27 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    
-    movie = {
-    "title": title,
-        "genre": genre,
-        "rating": rating
-    }
+    movie = {"title": title, "genre": genre, "rating": rating}
+
     if movie["title"] and movie["genre"] and movie["rating"]:
         return movie
-    else: return None
+    else:
+        return None 
 
+
+def add_to_watched(user_data, movie):
+    #user_data = {"watched": []}
+    #movie = create_movie()
+    user_data["watched"].append(movie)
+
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+
+
+    return user_data
+    
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
