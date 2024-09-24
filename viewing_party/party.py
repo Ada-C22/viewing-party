fc85e5f8eee21342d075542949868852f6bc393f
Tatyana Venanzi <tatyanavenanzi@gmail.com>
2024-09-24 15:55:59 -0700

    updated code for wave two

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -36,14 +36,15 @@ def watch_movie(user_data, title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 def get_watched_avg_rating(user_data):
-    num_of_ratings = 0
-    ratings_total = 0
     if not user_data["watched"]:
         return 0.0
-        
-    for i in range(len(user_data["watched"][0])):
+    
+    num_of_ratings = 0
+    ratings_total = 0
+    
+    for movie in user_data["watched"]:
+        ratings_total += movie["rating"]
         num_of_ratings += 1
-        ratings_total += user_data["watched"][0]["rating"]
     
     return ratings_total/num_of_ratings
     
