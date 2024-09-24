264eaa8b3de445ba771cd53b5d62f88b99a524d3
Madina-j <dzhetegenova@gmail.com>
2024-09-24 15:33:19 -0700

    implemented get_watched_avg_rating function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,3 +1,4 @@
+from copy import deepcopy 
 # ------------- WAVE 1 --------------------
 # No.1
 def create_movie(title, genre, rating):
@@ -17,18 +18,26 @@ def add_to_watched(user_data, movie):
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
-# No.4
+# No.4 -A
 def watch_movie(user_data,title):
     pass
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
-# No.1
+# No.1-M
 def get_watched_avg_rating(user_data):
-    pass
+    avg_rating = 0
+    ratings = 0
+    for movies in user_data.values():
+        for movie in movies:
+            rating = movie["rating"]
+            ratings += rating
+            avg_rating = ratings/ len(movies)
+    return avg_rating
 
-# No.2
+
+# No.2-A
 def get_most_watched_genre(user_data):
     pass
 
@@ -38,26 +47,26 @@ def get_most_watched_genre(user_data):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-# No.1
+# No.1-M
 def get_unique_watched(user_data):
     pass
-# No.2
+# No.2-A
 def get_friends_unique_watched(user_data):
     pass
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
-# No.1
+# No.1-M
 def get_available_recs(user_data):
     pass
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-# No.1
+# No.1-A
 def get_new_rec_by_genre(user_data):
     pass
 
-# No.2 
+# No.2-M
 def get_rec_from_favorites(user_data):
     pass
