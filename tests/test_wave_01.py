4e3a23eb12f5f700cf56de014111d9a3c3e2567d
Corntto9 <>
2024-09-23 23:00:32 -0700

    add wave 1 function 4 and test asserts

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -159,8 +159,8 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
-    
-    raise Exception("Test needs to be completed.")
+    assert any(movie["title"] == MOVIE_TITLE_1 for movie in updated_data["watched"])
+    #raise Exception("Test needs to be completed.")
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
@@ -184,7 +184,12 @@ def test_moves_movie_from_watchlist_to_watched():
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
     
-    raise Exception("Test needs to be completed.")
+    assert movie_to_watch in updated_data["watched"]
+    assert movie_to_watch not in updated_data["watchlist"]
+    
+    assert updated_data["watchlist"] == [FANTASY_1]
+    assert updated_data["watched"] == [FANTASY_2, movie_to_watch]
+    #raise Exception("Test needs to be completed.")
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -28,12 +28,17 @@ def add_to_watchlist(user_data, movie):
     return user_data
 
 def watch_movie(user_data,title):
-     
-
-
-
-
-     return user_data
+    movie_to_move = None
+    for movie in user_data['watchlist']:
+        if movie['title'] == title:
+            movie_to_move = movie
+            break
+          
+    if movie_to_move:
+        user_data['watchlist'].remove(movie_to_move)
+        user_data['watched'].append(movie_to_move)
+    
+    return user_data
           
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
