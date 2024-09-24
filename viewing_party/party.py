fb72097de7d62f44e20852ae23894d12cfa1dd36
Salma Anany <salmayousry5@gmail.com>
2024-09-23 21:24:52 -0700

    complete everything in wave one except tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -18,21 +18,7 @@ def add_to_watchlist(user_data: dict, movie: dict):
     movie_list.append(movie)
     return user_data
 
-# 4. Create a function named `watch_movie`. This function should...
-#
-# - take two parameters: `user_data`, `title`
-#   - the value of `user_data` will be a dictionary with a `"watchlist"` and a `"watched"`
-#     - This represents that the user has a watchlist and a list of watched movies
-#   - the value of `title` will be a string
-#     - This represents the title of the movie the user has watched
-# - If the title is in a movie in the user's watchlist:
-#   - remove that movie from the watchlist
-#   - add that movie to watched
-#   - return the `user_data`
-# - If the title is not a movie in the user's watchlist:
-#   - return the `user_data`
-#
-# Note: For Waves 2, 3, 4, and 5, your implementation of each of the functions should not modify `user_data`.
+
 def watch_movie(user_data, title):
     watchlist = user_data['watchlist']
     watched = user_data['watched']
