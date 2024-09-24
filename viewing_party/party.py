869c613d07122b71b34594b97c09e478890f4092
Leidy <leidymarbu@gmail.com>
2024-09-24 12:28:48 -0400

    #Create a copy of user_data in each func

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -20,28 +20,44 @@ def create_movie(movie_title, genre, rating):
 def add_to_watched(user_data, movie):
     # user_data is a DICT, ONE KEY: "watched": [list of dicts]
     # movie, is a single DICT with "title", "genre", "rating" keys
-    user_data["watched"].append(movie)
-    return user_data
+
+    #Crate a copy of user data to avoid modifying original LM
+    updated_user_data = {
+            "watched": user_data["watched"][:]
+        } 
+    
+    updated_user_data["watched"].append(movie)
+    return updated_user_data
+
 
 def add_to_watchlist(user_data, movie):
     # user_data is a DICT with key "watchlist": 
     #[movies user WANTS to watch]
-    user_data["watchlist"].append(movie)
-    return user_data
+
+    #Crate a copy of user data to avoid modifying original LM
+    updated_user_data = {
+            "watchlist": user_data["watchlist"][:],
+        } 
+    updated_user_data["watchlist"].append(movie)
+    return updated_user_data
 
 
 def watch_movie(user_data, title):
-    #pass
     # user_data = dict with
     # "watchlist" and "watched" keys
     # title is a str
 
-    for movie in user_data["watchlist"]:
+    #Crate a copy of user data to avoid modifying original LM
+    updated_user_data = {
+            "watchlist": user_data["watchlist"][:],
+            "watched": user_data["watched"][:]
+        } 
+
+    for movie in updated_user_data["watchlist"]:
         if movie["title"] == title:
-            user_data["watchlist"].remove(movie)
-            user_data["watched"].append(movie)
-            
-    return user_data
+            updated_user_data["watchlist"].remove(movie)
+            updated_user_data["watched"].append(movie)         
+    return updated_user_data
 
 
 
