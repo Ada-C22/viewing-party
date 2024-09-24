99a144f938624936210eb8fd4a6450514eb3325d
Elzara Turakulova <endjik@gmail.com>
2024-09-24 13:42:37 -0700

    Complete wave 4 function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -97,7 +97,16 @@ def get_friends_unique_watched(user_data):
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+def get_available_recs(user_data):
 
+    unique_movies = get_friends_unique_watched(user_data)
+
+    recommended_movies = []
+    for movie in unique_movies:
+        host = movie["host"]
+        if host in user_data["subscriptions"]:
+            recommended_movies.append(movie)
+    return recommended_movies
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
