0ce1ceadde7bb042a5024197f59e6c56060faaea
Tami <tamigaertner@outlook.com>
2024-09-25 16:43:32 -0400

    work and fix bugs for wave 4

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,60 +1,5 @@
-HORROR_1 = {
-    "title": "MOVIE_TITLE_1",
-    "genre": "GENRE_1",
-    "rating": "RATING_1"
-}
-FANTASY_1 = {
-    "title": "The Lord of the Functions: The Fellowship of the Function",
-    "genre": "Fantasy",
-    "rating": 4.8
-}
-FANTASY_2 = {
-    "title": "The Lord of the Functions: The Two Parameters",
-    "genre": "Fantasy",
-    "rating": 4.0
-}
-FANTASY_3 = {
-    "title": "The Lord of the Functions: The Return of the Value",
-    "genre": "Fantasy",
-    "rating": 4.0
-}
-FANTASY_4 = {
-    "title": "The Programmer: An Unexpected Stack Trace",
-    "genre": "Fantasy",
-    "rating": 4.0
-}
-ACTION_1 = {
-    "title": "The JavaScript and the React",
-    "genre": "Action",
-    "rating": 2.2
-}
-ACTION_2 = {
-    "title": "2 JavaScript 2 React",
-    "genre": "Action",
-    "rating": 4.2
-}
-ACTION_3 = {
-    "title": "JavaScript 3: VS Code Lint",
-    "genre": "Action",
-    "rating": 3.5
-}
-INTRIGUE_1 = {
-    "title": "Recursion",
-    "genre": "Intrigue",
-    "rating": 2.0
-}
-INTRIGUE_2 = {
-    "title": "Instructor Student TA Manager",
-    "genre": "Intrigue",
-    "rating": 4.5
-}
-INTRIGUE_3 = {
-    "title": "Zero Dark Python",
-    "genre": "Intrigue",
-    "rating": 3.0
-}
 # ------------- WAVE 1 --------------------
-#
+
 def create_movie(title, genre, rating):
     
     if title and genre and rating:
@@ -73,26 +18,6 @@ def add_to_watched(user_data, movie):
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
-    # print(user_data)   
-
-# usr = {
-#         "watched": [
-#             "FANTASY_1",    
-#             "FANTASY_3",
-#             "FANTASY_4",
-#             "HORROR_1",
-#         ]
-#     ,
-#         "watchlist": [
-#             "FANTASY_2",
-#             "ACTION_2",
-#             "INTRIGUE_2",
-#             "INTRIGUE_2",
-#         ]
-#     }
-
-# add_to_watchlist( usr, "soe " )
-
 
 def watch_movie(user_data, title):
     for movie in user_data["watchlist"]:
@@ -136,12 +61,11 @@ def get_most_watched_genre(user_data):
               
     most_watched = max(count_genres, key= count_genres.get)
     return most_watched
-      
-                                
-        
+                                         
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+
 def get_unique_watched(user_data):
     user_watched_movies = user_data["watched"][:]
     friends = user_data["friends"]
@@ -166,35 +90,18 @@ def get_friends_unique_watched(user_data):
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+
 def get_available_recs(user_data):
-    # find the recommeded movies. ONY add to the list if ALL are true
-    # user has not watched it
-    # at least one friend has watched it
-    # AND the host is under the users subscription
-    # retun recommended movies
-    # for the logig all must be true
-    # if user movies NOT watched AND friend == watched AND host is under user subcription. 
-    
-    recommended_titles = []
-    user_watched = set()
-    for movie in user_data["watched"]:
-        title = movie["title"]
-        user_watched.add(title)
-        
-    for friends in user_data["friends"]:
-        for movie in friends["watched"]:
-            if (movie["title"] not in user_watched and movie["host"] in user_data["subscriptions"]):
-                recommended_titles.append(movie)
-               
-    return recommended_titles
-            
-            
-        
-    
-       
-                
-   
            
+    recommended_title = []
+    
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie not in user_data["watched"] and movie["host"] in user_data["subscriptions"]:
+                if movie not in recommended_title:
+                    recommended_title.append(movie)
+    return recommended_title
+                  
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
