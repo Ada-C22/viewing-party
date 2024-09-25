726bde663b163be492ebc218c2a145cad38d7b83
Kristina <hngyn.kristina@gmail.com>
2024-09-24 17:31:50 -0700

    wave 3 function 1 finished

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -36,22 +36,66 @@ def watch_movie(user_data, title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 def get_watched_avg_rating(user_data):
-    num_of_ratings = 0
-    ratings_total = 0
     if not user_data["watched"]:
         return 0.0
+    
+    num_of_ratings = 0
+    ratings_total = 0
         
-    for i in range(len(user_data["watched"][0])):
+    # for i in range(len(user_data["watched"][0])):
+    #     num_of_ratings += 1
+    #     ratings_total += user_data["watched"][0]["rating"]
+
+    for movie in user_data["watched"]:
+        ratings_total += movie["rating"]
         num_of_ratings += 1
-        ratings_total += user_data["watched"][0]["rating"]
     
     return ratings_total/num_of_ratings
+
+def get_most_watched_genre(user_data):
+    if not user_data["watched"]:
+        return None
     
+    genre_frequency = {}
+
+    #first for loop tracks amount of times a genre appears
+    for movie in user_data["watched"]:
+        genre_type = movie["genre"]
+        if genre_type in genre_frequency:
+            genre_frequency[genre_type] += 1
+        else:
+            genre_frequency[genre_type] =1
+    
+    most_watched_genre = None
+    highest_genre_count = 0
+
+    #second for loop picks out which genre is the most watched
+    for genre, count in genre_frequency.items():
+        if count > highest_genre_count:
+            highest_genre_count = count
+            most_watched_genre = genre
+
+    return most_watched_genre
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+def get_unique_watched(user_data):
+    unique_movies_list = []
+    total_friends_watched = []
 
+    for movies in user_data["friends"]:
+        total_friends_watched += movies["watched"]
+
+    for unique_movies in user_data["watched"]:
+        if unique_movies not in total_friends_watched:
+            unique_movies_list.append(unique_movies)
+    
+    return unique_movies_list
+
+
+def get_friends_unique_watched(user_data):
+    pass
 
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
