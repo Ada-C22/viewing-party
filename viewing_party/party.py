a8360cb78314f1cc2edf7a715f01dec8b004c1a7
Aleida Vieyra <avieyra@ufl.edu>
2024-09-25 18:25:52 -0400

    pushing changes to Wave 3 and 4, coauthored with Leidy Martinez

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -104,13 +104,85 @@ def get_most_watched_genre(user_data):
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+def get_unique_watched(user_data):
+    #ONLY THE USER HAS WATCHED, BUT THE FRIENDS HAVE NOT
+    user_unique_watched = []
+    friends_watched = []
+
+    for friend in user_data["friends"]:
+        for movie in friend['watched']:
+            friends_watched.append(movie['title'])
+    
+    for movie in user_data["watched"]:
+        if movie['title'] not in friends_watched:
+            user_unique_watched.append(movie)
+
+    return user_unique_watched
+
+
+def get_friends_unique_watched(user_data):
+    
+    # user_watched = [movie for movie in user_data["watched"]]
+    user_watched = []
+    friends_unique_watched = []
+
+    for movie in user_data["watched"]:
+        user_watched.append(movie)
+
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            # print(f"Current movie title is: {movie["title"]}")
+            # print(friends_unique_watched)
+            if movie not in user_watched and movie not in friends_unique_watched:
+                friends_unique_watched.append(movie)
+
+    # print(friends_unique_watched)
+    return friends_unique_watched
 
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
 
+def get_available_recs(user_data):
+    
+    # user_data = {
+    # "subsriptions" : ["streaming service"],
+    # "friends": [{"watched":[{"host":"streaming service"}]}]
+    # }
+
+# Determine list of recommended movies:
+    # only if user has NOT watched it.
+    # at least one of the user's friends HAS watched it
+    # "host" of the movie is a service that is in the user's
+        # "subscriptions"
+    #friends_unique_movies = [movie for movie in get_friends_unique_watched(user_data)]
+    friends_unique_movies = []
+    rec_list = []
+
+    for movie in get_friends_unique_watched(user_data):
+        friends_unique_movies.append(movie)
+
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie in rec_list:
+                continue
+            elif movie["host"] in user_data["subscriptions"] and movie in friends_unique_movies:
+                rec_list.append(movie)
+    
+
+    print(f"The final recommendations list is: {rec_list}")
+    return rec_list
+
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
+
+def get_new_rec_by_genre(user_data):
+    pass
+
+def get_rec_from_favorites(user_data):
+
+    pass
+
 
