3ba858a5ddda13d3f13fc90e3d2aab484068d690
Tatyana Venanzi <tatyanavenanzi@gmail.com>
2024-09-25 15:08:25 -0700

    made wave 4 and passed all tests during co-working time with Kristina

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -38,8 +38,16 @@ def watch_movie(user_data, title):
 def get_watched_avg_rating(user_data):
     if not user_data["watched"]:
         return 0.0
+    
+    num_of_ratings = 0
+    ratings_total = 0
         
-    for i in range(len(user_data["watched"][0])):
+    # for i in range(len(user_data["watched"][0])):
+    #     num_of_ratings += 1
+    #     ratings_total += user_data["watched"][0]["rating"]
+
+    for movie in user_data["watched"]:
+        ratings_total += movie["rating"]
         num_of_ratings += 1
     
     return ratings_total/num_of_ratings
@@ -72,11 +80,106 @@ def get_most_watched_genre(user_data):
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+def get_unique_watched(user_data):
+    unique_movies_list = []
+    total_friends_watched = []
 
+    for movies in user_data["friends"]:
+        total_friends_watched += movies["watched"]
+
+    for unique_movies in user_data["watched"]:
+        if unique_movies not in total_friends_watched:
+            unique_movies_list.append(unique_movies)
+    
+    return unique_movies_list
+
+
+def get_friends_unique_watched(user_data):
+    unique_movies_list = []
+    total_friends_watched = []
+
+# looping through all movies watched by friend
+    for friend in user_data["friends"]:
+        total_friends_watched += friend["watched"]
+
+
+
+# loop through user's watched movies and compare titles
+    for unique_movies in user_data["watched"]:
+        if unique_movies["title"] not in total_friends_watched:
+            unique_movies_list.append(unique_movies)
+
+    return unique_movies_list
+
+
+# MOVIE_TITLE_1 = "It Came from the Stack Trace"
+# GENRE_1 = "Horror"
+# RATING_1 = 3.5
+
+# HORROR_1 = {
+#     "title": MOVIE_TITLE_1,
+#     "genre": GENRE_1,
+#     "rating": RATING_1
+# }
+
+# FANTASY_1 = {
+#     "title": "The Lord of the Functions: The Fellowship of the Function",
+#     "genre": "Fantasy",
+#     "rating": 4.8
+# }
+
+# INTRIGUE_1 = {
+#     "title": "Recursion",
+#     "genre": "Intrigue",
+#     "rating": 2.0
+# }
+
+# amandas_data = {
+#         "watched": [
+#             HORROR_1,
+#             FANTASY_1
+#         ],
+#         "friends": [
+#             {
+#                 "watched": [
+#                     HORROR_1,
+#                     FANTASY_1,
+#                     INTRIGUE_1
+#                 ]
+#             },
+#             {
+#                 "watched": [
+#                     INTRIGUE_1
+#                 ]
+#             }
+#         ]
+#     }
+
+# print(get_friends_unique_watched(amandas_data))
 
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+
+def get_available_recs(user_data):
+    # list titles of movies the user has already watched 
+    user_watched_list = []
+    for movie in user_data["watched"]:
+        user_watched_list.append(movie["title"])
+    
+    # list that has rec movies
+    recommendations = []
+
+    # loop through each friend
+    for friend in user_data["friends"]:
+        # loop through their moviees
+        for friend_movie in friend["watched"]:
+            # check if user hasn't watched the movie and if host (streaming services) is also under user
+            if (friend_movie["title"] not in user_watched_list and friend_movie["host"] in user_data["subscriptions"] and friend_movie not in recommendations):
+                recommendations.append(friend_movie)
+
+    return recommendations
+
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
