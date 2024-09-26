09cf5249ce5eaa0ca712df09da3c4947579bdf71
Natalie <natali3sen@gmail.com>
2024-09-25 19:28:41 -0500

    wave 3 with 1 failing (unwritten)test

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -85,6 +85,71 @@ def get_most_watched_genre(user_data):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
+def get_unique_watched(user_data):
+    user_watched = user_data["watched"]
+    user_watched_titles = set()
+    for movie in user_watched:
+        user_watched_titles.add(movie["title"])
+    
+    friends_watched_titles = set()
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            friends_watched_titles.add(movie["title"])
+
+    unique_watched = []
+    for movie in user_watched:
+        if movie["title"] not in friends_watched_titles:
+            unique_watched.append(movie)
+    return unique_watched
+
+def get_friends_unique_watched(user_data):
+    # get the list of titles that the user has watched
+    # create a set for titles friends have watched
+    # find unique movies friends have watched that the user hasn't watched
+    # initialized an empty set to store the titles that the user has watched
+    # use for loop to iterate over each movie dicts in the "watched" list
+    user_watched_titles = set()
+    for movie in user_data["watched"]:
+        user_watched_titles.add(movie["title"])
+    
+    #initialize an empty set to store the movie titles friends have watched
+    # use for loop to literate over each friend in the list "friends" in the dict
+    # use another for loop to iterate over each movie in the "watched" dict in friends list
+    # then add the movie titles to the set
+    friends_watched_titles = set()
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            friends_watched_titles.add(movie["title"])
+
+    # initilize ab enpty list to store the unique movies
+    # use for loop to iterate over each title in friends_watched_titles
+    # check if the title is not in user_watched_titles
+    # break once the movie/movies are found
+    
+    unique_friends_watched = []
+    for title in friends_watched_titles:
+        if title not in user_watched_titles:
+            for friend in user_data["friends"]:
+                for movie in friend["watched"]:
+                    if movie["title"] == title:
+                        unique_friends_watched.append(movie)
+                        break     
+    
+    return unique_friends_watched
+
+
+
+
+    
+
+
+
+    
+
+
+
+        
+    
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
