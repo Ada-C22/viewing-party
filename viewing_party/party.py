0a2186bea50e7b6589160baaeb156bd1d2e2600a
Corntto9 <>
2024-09-25 23:09:25 -0700

    commit before pull

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -61,6 +61,7 @@ def get_most_watched_genre(user_data):
 
     watched_movie = user_data.get('watched',[])
     
+    #count occurences of each genre
     genre_count = {}
     most_watched_genre = ""
     max_count = 0
@@ -92,6 +93,64 @@ def get_most_watched_genre(user_data):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
+a = {'watched': [{'title': 'The Lord of the Functions: The Fellowship of the Function', 'genre': 'Fantasy', 'rating': 4.8}, 
+             {'title': 'The Lord of the Functions: The Two Parameters', 'genre': 'Fantasy', 'rating': 4.0}, 
+             {'title': 'The Lord of the Functions: The Return of the Value', 'genre': 'Fantasy', 'rating': 4.0}, 
+             {'title': 'The JavaScript and the React', 'genre': 'Action', 'rating': 2.2}, 
+             {'title': 'Recursion', 'genre': 'Intrigue', 'rating': 2.0}, 
+             {'title': 'Instructor Student TA Manager', 'genre': 'Intrigue', 'rating': 4.5}],
+  'friends': [
+    {'watched': [
+        {'title': 'The Lord of the Functions: The Fellowship of the Function', 'genre': 'Fantasy', 'rating': 4.8}, 
+        {'title': 'The Lord of the Functions: The Return of the Value', 'genre': 'Fantasy', 'rating': 4.0}, 
+        {'title': 'The Programmer: An Unexpected Stack Trace', 'genre': 'Fantasy', 'rating': 4.0}, 
+        {'title': 'It Came from the Stack Trace', 'genre': 'Horror', 'rating': 3.5}]}, 
+    {'watched': [
+        {'title': 'The Lord of the Functions: The Fellowship of the Function', 'genre': 'Fantasy', 'rating': 4.8}, 
+        {'title': 'The JavaScript and the React', 'genre': 'Action', 'rating': 2.2}, 
+        {'title': 'Recursion', 'genre': 'Intrigue', 'rating': 2.0}, 
+        {'title': 'Zero Dark Python', 'genre': 'Intrigue', 'rating': 3.0}]
+        }
+    ]
+}
+
+#get a list of title in watched 
+#get a list of title in friends 
+#compare two list
+
+
+
+
+def get_friends_unique_watched(user_data):
+    friends_unique_watched = {}
+    
+
+   
+    user_watched_movie = user_data.get('watched',[])
+    print(user_watched_movie)
+
+    #for movie in user_data['watched']:
+    #    user_watched_movie_titles.append(movie['title'])
+    
+    #print(user_watched_movie_titles)
+
+    print("---------")
+
+    friends_watched_movie_titles = []
+    for movie in user_data['friends']:
+        #friends_watched_movie_titles.append(movie['title'])
+        friends_watched_movie = movie['watched']
+    
+    friends_unique_watched = set(friends_watched_movie) - set(user_watched_movie)
+
+    print("---------")
+    print(friends_watched_movie)
+    
+    
+
+   
+    
+    return friends_unique_watched
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
