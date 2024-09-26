835be767011f0745a327a322b2985954ce8db968
Christelle Nkera <scnkera@gmail.com>
2024-09-26 17:36:00 -0400

    Wave 1: Added an assertion

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -188,6 +188,7 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
+    assert updated_data["watched"][-1] == movie_to_watch
     
     # if updated_data["watched"][1] != FANTASY_1:
     # raise Exception("Test needs to be completed.")
