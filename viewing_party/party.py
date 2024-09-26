1268b2ce9ef31e3ce2a3d392692fa01d780004a5
Corntto9 <>
2024-09-26 13:48:29 -0700

    add wave 5 function 2, did formatting

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -22,6 +22,7 @@ def add_to_watchlist(user_data, movie):
     user_data['watchlist'].append(movie)
     return user_data
 
+
 def watch_movie(user_data,title):
     movie_to_move = None
     for movie in user_data['watchlist']:
@@ -56,7 +57,8 @@ def get_watched_avg_rating(user_data):
 
     average_rating = rating_sum / total_movies
     return average_rating
-        
+
+
 def get_most_watched_genre(user_data):
 
     watched_movie = user_data.get('watched',[])
@@ -127,20 +129,13 @@ def get_friends_unique_watched(user_data):
 # ------------- WAVE 4 --------------------
 
 def get_available_recs(user_data):
-
-   
-
     user_subscriptions = set(user_data.get('subscriptions', []))
     friends_recommendadtions = get_friends_unique_watched(user_data)
 
     return [movie for movie in friends_recommendadtions if movie['host'] in user_subscriptions]
 
 
-
-
-# -----------------------------------------
 # ------------- WAVE 5 --------------------
-# -----------------------------------------
 
 def get_new_rec_by_genre(user_data):
     recommended_movies_list = []
@@ -154,12 +149,21 @@ def get_new_rec_by_genre(user_data):
     
     return recommended_movies_list
 
-def get_new_rec_by_genre(user_date):
+
+def get_rec_from_favorites(user_data):
     recommend_movies_list = []
-    # get user title from favorite
-    # 
 
-    # get friends watech  
+    user_favorites_watched_movies = user_data.get('favorites', [])
 
+    friends_watched_movies_list = user_data.get('friends',[])
+   
+    friends_watched_movies_title_list = []
+    for friend_watched_movies in friends_watched_movies_list:
+        for friend_movie in friend_watched_movies.get('watched',[]):
+            friends_watched_movies_title_list.append(friend_movie['title'])
+    for movie in user_favorites_watched_movies:
+        if movie['title'] not in friends_watched_movies_title_list:
+            recommend_movies_list.append(movie)
+ 
     return recommend_movies_list
     
