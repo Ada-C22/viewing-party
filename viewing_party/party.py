48af3b4e943016fcef369df9842965dca8726e57
Tatiana Trofimova <nerpassevera@gmail.com>
2024-09-26 12:55:22 -0700

    refactor: removes excess whitespaces

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -217,42 +217,42 @@ def watch_movie(user_data, title):
             user_data["watched"].append(movie)
             break 
     return user_data
- 
-    
+
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
 def get_watched_avg_rating(user_data):
-    
+
     total_ratings = 0
     number_of_movies = 0
     for movie in user_data["watched"]:
         total_ratings += movie["rating"]
         number_of_movies += 1
         average_rating = total_ratings / number_of_movies
-        
+
     if number_of_movies == 0:
         return 0.0
     return average_rating
 
 def get_most_watched_genre(user_data):
-    
+
     if user_data['watched'] == []:
         return None
-    
+
     count_genres = {}
-    
+
     for movie in user_data["watched"]:
         genre = movie["genre"]
         if genre in count_genres:
             count_genres[genre] +=1
         else:
             count_genres[genre] =1
-              
+
     most_watched = max(count_genres, key= count_genres.get)
     return most_watched
-                                         
+
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
@@ -283,16 +283,16 @@ def get_friends_unique_watched(user_data):
 # -----------------------------------------
 
 def get_available_recs(user_data):
-           
+
     recommended_title = []
-    
+
     for friend in user_data["friends"]:
         for movie in friend["watched"]:
             if movie not in user_data["watched"] and movie["host"] in user_data["subscriptions"]:
                 if movie not in recommended_title:
                     recommended_title.append(movie)
     return recommended_title
-                  
+
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
@@ -306,7 +306,7 @@ def get_new_rec_by_genre(user_data):
             genre_count[movie["genre"]] += 1
         else:
             genre_count[movie["genre"]] = 1
-    
+
     fav_genre = ""
     fav_genre_count = 0
     for genre, count in genre_count.items():
@@ -332,11 +332,8 @@ def get_rec_from_favorites(user_data):
     for movie in friends_watched:
         if movie in favorites:
             favorites.remove(movie)
-    
+
     print(f"{favorites=}", "\n")
     return favorites
 
 print(get_rec_from_favorites(USER_DATA_5))
-
-
-
