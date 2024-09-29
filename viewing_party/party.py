6de27444df969d6b5a0f10874979857960e5dea7
Madeline Bennett <madeline.bennett.pm@gmail.com>
2024-09-29 22:47:17 +1000

    wave 5 all tests passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -38,6 +38,7 @@ def watch_movie(user_data, movie):
             watched_list.append(to_watch_movie)
     return user_data
 
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
@@ -111,6 +112,15 @@ def get_friends_unique_watched(user_data):
                 unique_watched.append(friends_movie)
     return unique_watched
 
+def join_friends_list(user_data):
+    friends_list = user_data.get("friends")
+    friends_movie_list = []
+    for friend in friends_list:
+        for movie in friend["watched"]:
+            friends_movie_list.append(movie)
+    return friends_movie_list
+
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
@@ -124,12 +134,35 @@ def get_available_recs(user_data):
             movie_recs.append(movie)
     return movie_recs
 
-
-
-
-
-
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 
+def get_new_rec_by_genre(user_data):
+    new_rec_by_genre = []
+    joined_friends_list = join_friends_list(user_data)
+    if user_data["watched"] == [] or joined_friends_list == []:
+        return new_rec_by_genre
+    
+
+    most_watched_genre = get_most_watched_genre(user_data)
+    unreviewed_recs = get_available_recs(user_data)
+    for movie in unreviewed_recs:
+        movie_genre = movie["genre"]
+        if movie_genre == most_watched_genre:
+            new_rec_by_genre.append(movie)
+
+    return new_rec_by_genre
+
+def get_rec_from_favorites(user_data):
+    favorites = user_data["favorites"]
+    joined_friends_list = join_friends_list(user_data)
+    rec_from_favorites_list = []
+
+    print(joined_friends_list)
+    for movie in favorites:
+        if movie not in joined_friends_list: 
+            rec_from_favorites_list.append(movie)
+            print(movie)
+
+    return rec_from_favorites_list
