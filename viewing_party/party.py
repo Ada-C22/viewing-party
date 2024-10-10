28d7f99332a1fea6a2f26122d5efc9f06ec0469a
Marjana Khatun <marjana1816@gmail.com>
2024-10-10 00:52:19 -0700

    Done wave2 part2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -20,6 +20,7 @@ def add_to_watched(user_data, movie):
 def add_to_watchlist(user_data,movie):
     user_data["watchedlist"] = movie
     return user_data
+
 def watch_movie(user_data, title):
     if title in user_data["watchlist"]:
         user_data["watchlist"].remove(title)
@@ -41,12 +42,35 @@ def get_watched_avg_rating(user_data):
         for movies in value:
             no_movies += 1
 
-    avg_rating = sum_rating/no_movies
-
+    if no_movies == 0:
+        avg_rating = 0.0
+    else:
+        avg_rating = sum_rating/no_movies
     
-    
-
     return avg_rating
+
+def get_most_watched_genre(user_data):
+    genre_list = []
+    for value in user_data.values():
+        for movies in value:
+            genre_list.append(movies["genre"])
+
+    dict = {}
+    for item in genre_list:
+        if item in dict:
+            dict[item] += 1
+        else:
+            dict[item] = 1
+    
+    max_value = 0
+    max_key = None
+
+    for key, value in dict.items():
+        if value > max_value:
+            max_value = value
+            max_key = key
+    return max_key
+
     
 
 
