5d4e355851a30b9691ddbe0f978c897c0747b735
marjanak <marjana1816@gmail.com>
2024-10-24 13:58:38 -0700

    Completed all the test asseertions

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -158,6 +158,8 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
+    movie = updated_data ["watched"][0]
+    assert MOVIE_TITLE_1 == movie["title"]
    
     
     #raise Exception("Test needs to be completed.")
@@ -183,7 +185,8 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
-    #assert len(updated_data) ["watchedlist"] ==
+    assert movie_to_watch == HORROR_1
+    
     #raise Exception("Test needs to be completed.")
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
